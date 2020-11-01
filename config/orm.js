d7f2e0b6658f473950168c923b6f533aca8615ed
aidabyte <ablinstrubyte@gmail.com>
2020-10-31 22:52:33 -0500

    writing in my method queries

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,27 @@
 // Importing MySQL connection.
 var connection = require("../config/connection.js");
+
+
+var orm = {
+    selectAll:function(tableInput,cb) {
+        var queryString = `SELECT * FROM ${tableInput};`
+        connection.query(queryString, function (err, result) {
+           if (err) {
+               throw err;
+           } 
+           cb(result);
+        });
+    },
+
+
+    insertOne: function(tableInput, cols, vals, cb) {
+        var queryString = `SELECT * FROM ${tableInput} ${cols} ${vals};`
+        connection.query(queryString, function (err, result) {
+           if (err) {
+               throw err;
+           } 
+           cb(result);
+        });
+    }
+
+}
