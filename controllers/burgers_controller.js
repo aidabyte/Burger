28240f31db74df3fb16382d86995ed4a45f770a5
aidabyte <ablinstrubyte@gmail.com>
2020-11-01 16:23:26 -0600

    get and post routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,9 +2,26 @@ var express = require("express");
 
 var router = express.Router();
 
+var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+      var bObject = {
+        burgers: data
+      };
+      console.log(bObject);
+      res.render("index", bObject);
+    });
+  });
 
-
+  router.post("/api/burgers", function(req, res) {
+    burger.insertOne(
+      req.body.burger_name
+    , function(result) {
+      
+      res.json(result);
+    });
+  });
 
 
 
