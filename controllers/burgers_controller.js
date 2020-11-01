0665f9e61a9a5bd6f8f7455d526fca587e2d5bd4
aidabyte <ablinstrubyte@gmail.com>
2020-11-01 16:34:00 -0600

    added the update

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -23,6 +23,22 @@ router.get("/", function(req, res) {
     });
   });
 
+  router.put("/api/burgers/devoured/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+  
+   
+  
+    burger.update({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  });
 
 
 
