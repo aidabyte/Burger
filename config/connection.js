c4cdb86d34a9fb0a6a2c6db75740167ef5fb84fb
aidabyte <ablinstrubyte@gmail.com>
2020-11-01 20:16:13 -0600

    changed connection

--- config/connection.js
+++ config/connection.js
@@ -5,10 +5,6 @@ var mysql = require("mysql");
 // Create express app instance.
 var app = express();
 
-// Set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var PORT = process.env.PORT || 8080;
-
 // MySQL DB Connection Information (remember to change this with our specific credentials)
 var connection = mysql.createConnection({
   host: "localhost",
