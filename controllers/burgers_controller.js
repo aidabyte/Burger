996408c28b0c26ca9a745d3b44b83490703bae1f
aidabyte <ablinstrubyte@gmail.com>
2021-01-11 22:21:36 -0600

    changes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,9 +3,9 @@ var express = require("express");
 var router = express.Router();
 
 var burger = require("../models/burger.js");
-
+// creating all of the routes that are required 
 router.get("/", function(req, res) {
-    burger.all(function(data) {
+    burger.selectAll(function(data) {
       var bObject = {
         burgers: data
       };
@@ -15,20 +15,24 @@ router.get("/", function(req, res) {
   });
 
   router.post("/api/burgers", function(req, res) {
-    burger.insertOne(
-      req.body.burger_name
+    burger.insertOne([
+      "burger_name", "devoured"
+    ],
+      req.body.burger_name , req.body.devoured
     , function(result) {
       
-      res.json(result);
+      res.json({id:result.insertId});
     });
   });
 
-  router.put("/api/burgers/devoured/:id", function(req, res) {
+  router.put("api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
+
+    console.log(condition)
   
    
   
-    burger.update({
+    burger.updateOne({
       devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
@@ -40,6 +44,14 @@ router.get("/", function(req, res) {
     });
   });
 
+  router.delete("/api/burgers/:id", function(req,res){
+    if (result.changedRows === 0) {
+
+      return res.status(404).end();
+    }else {
+      res.status(200).end();
+    }
+    })
 
 
 module.exports = router;
